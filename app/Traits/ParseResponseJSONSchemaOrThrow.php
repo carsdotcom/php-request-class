3e4b70ae6eb0094cbacb439bc36faec28481a103
Benjamin Benesh <bbenesh@dealerinspire.com>
2024-12-10 13:29:23 -0600

    fix: use validateEncodedStringOrThrow instead for ParseResponseJSONSchemaOrThrow

--- app/Traits/ParseResponseJSONSchemaOrThrow.php
+++ app/Traits/ParseResponseJSONSchemaOrThrow.php
@@ -40,13 +40,13 @@ trait ParseResponseJSONSchemaOrThrow
             );
         }
 
-        SchemaValidator::validateOrThrow(
-            $parsed,
-            static::RESPONSE_SCHEMA,
-            'Unexpected problem with ' .
+        SchemaValidator::validateEncodedStringOrThrow(
+            data: $parsed,
+            schema: static::RESPONSE_SCHEMA,
+            exceptionMessage: 'Unexpected problem with ' .
             Helpers::friendlyClassName(static::class) .
             ' call: Response does not match expected schema',
-            failureHttpStatusCode: Response::HTTP_BAD_GATEWAY, // status code blames upstream API, we are but a gateway
+            failureHttpStatusCode: Response::HTTP_BAD_GATEWAY // status code blames upstream API, we are but a gateway
         );
         return $parsed;
     }
